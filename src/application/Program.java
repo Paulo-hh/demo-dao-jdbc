bc1527411b40919b4b1e587e2a863733197d589a
Paulo Hoff <paulohhoff@gmail.com>
2024-10-28 21:43:28 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.parse("25/04/2023", fmt), 3000.0, obj);
+		System.out.println(seller);
 		
 	}
 
