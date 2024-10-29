503c14070374e4c036c60e68a87650269b4c11ee
Paulo Hoff <paulohhoff@gmail.com>
2024-10-28 21:57:47 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,15 +3,21 @@ package application;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
 		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		Department obj = new Department(1, "Books");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.parse("25/04/2023", fmt), 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 		
 	}
